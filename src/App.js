b90a0d01c55e4b70256d5f453856f1b702429f98
whereisjw <j9399977@gmail.com>
2023-12-28 19:40:30 +0900

    12/28 초기세팅 front commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+  <Outlet/>
+  );
 }
 
 export default App;
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <div className="text-font text-9xl hover:text-main">메인페이지</div>
+    </div>
+  );
+};
+
+export default Main;
