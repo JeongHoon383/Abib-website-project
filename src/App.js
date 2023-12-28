06e3ef566a9435c269b16146bef6b4c53eed5f0e
naginooi <xxxkmsz@naver.com>
2023-12-28 18:10:52 +0900

    1228 settings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <></>;
+  return <div className="text-3xl text-red-300">Hello World</div>;
 }
 
 export default App;
