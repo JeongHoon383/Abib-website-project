46101eaf9d28c52b6be2cc86f77b6d9223fe2754
DESKTOP-NRC9BD9\User <rkawk2239@naver.com>
2023-12-29 17:32:56 +0900

    12/19 초기세팅 혁진

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <></>;
+  return <div className="text-3xl text-red-400">hello world</div>;
 }
 
 export default App;
