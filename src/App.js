59583ea21bf7ffeba9f9fae5cee132974653b6c3
Jeong Hoon <gbg9609@naver.com>
2023-12-29 17:40:11 +0900

    12/29 테스트 정훈

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="hello text-red-300">hello world</div>;
+  return (
+    <div className="hover:opacity-75 opacity-50 hover:scale-150 scale-125">
+      hello world
+    </div>
+  );
 }
 
 export default App;
