3c91880c60abc2982cb98e3af557c18675086923
naginooi <xxxkmsz@naver.com>
2023-12-29 10:13:03 +0900

    12/28

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        font: '#2f3640',
+        back: '#f5f6fa',
+        main: '#00a8ff',
+        accent: '#0097e6',
+      },
+    },
   },
+  darkMode: 'class',
   plugins: [],
 };
