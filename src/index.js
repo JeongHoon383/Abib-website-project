0c16d25de8545257442de54418f6d1d67e9997d7
naginooi <xxxkmsz@naver.com>
2023-12-30 23:11:46 +0900

    12/30 search result 테스트 지수

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Main from './Pages/Main';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./Pages/Main";
+import SearchResult from "./Pages/Search/SearchResult";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <div>not found</div>,
     children: [
       {
-        index: '/',
+        index: "/",
         element: <Main />,
       },
       {
-        path: '/product',
+        path: "/product",
         element: <div>product</div>,
+      },
+      {
+        path: "/search",
+        element: <SearchResult />,
       },
     ],
   },
 ]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
