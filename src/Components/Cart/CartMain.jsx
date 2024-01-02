643a1f1790cb49fff5545a8cd36e448433eae35a
Jeong Hoon <gbg9609@naver.com>
2024-01-02 23:44:18 +0900

    1/2 cart-main-ui hoon

--- src/Components/Cart/CartMain.jsx
+++ src/Components/Cart/CartMain.jsx
@@ -4,17 +4,17 @@ import CartProduct from "./CartProduct";
 const CartMain = () => {
   return (
     <div>
-      <div className="mx-auto max-w-[900px] border border-solid border-red-300 pt-[90px]">
+      <div className="mx-auto max-w-[900px] pt-[90px] text-xs">
         <div>
           <div>
-            <ul className="tex flex">
-              <li className="w-[20px] px-[5px] py-[12px]">
+            <ul className="flex w-full items-center justify-between border-b border-solid border-gray-300 font-bold">
+              <li className="px-[5px] py-[12px]">
                 <input
-                  className="mx-[3px] mb-[7px] mt-[5px] box-border h-[14px] w-[14px] flex-none rounded-none border border-solid border-gray-300"
+                  className="mx-[3px] mb-[7px] mt-[5px] flex-1"
                   type="checkbox"
                 />
               </li>
-              <li className="w-[104px]"></li>
+              <li></li>
               <li>PRODUCT</li>
               <li>UNIT PRICE</li>
               <li>QUANTITY</li>
@@ -25,22 +25,38 @@ const CartMain = () => {
             </ul>
           </div>
           <CartProduct />
-          <div>
-            <div>선택삭제</div>
+          <div className="flex flex-row-reverse px-[10px] pb-[17px] pt-[15px] underline">
             <div>장바구니비우기</div>
+            <div className="mr-[15px]">선택삭제</div>
           </div>
-          <div>
-            <div>
-              상품구매금액 48,000 + 배송비 2,500 - 상품할인금액 14,400 = $36,100
-            </div>
+          <div className="mb-[50px]">
+            <ul className="flex justify-center px-[10px] pb-[17px] pt-[15px]">
+              <li>상품구매금액 48,000</li>
+              <li className="ml-[5px] mr-[5px]">+</li>
+              <li>배송비 2,500</li>
+              <li className="ml-[5px] mr-[5px]">-</li>
+              <li>상품할인금액 14,400</li>
+              <li className="ml-[5px] mr-[5px]">=</li>
+              <li className="font-bold">₩36,100</li>
+            </ul>
           </div>
-          <div>
-            <div>총 할인금액 상세내역</div>
+          <div className="flex flex-col items-center justify-center">
+            <div className="font-bold">총 할인금액 상세내역</div>
             <div>$7,200원</div>
             <div>기간할인 $7,200</div>
           </div>
         </div>
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
--- src/Components/Cart/CartProduct.jsx
+++ src/Components/Cart/CartProduct.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 
 const CartProduct = () => {
-  return <div>컴포넌트</div>;
+  return (
+    <div>
+      <ul className="flex w-full items-center border-b border-solid border-gray-300 py-[12px] text-xs">
+        <li>
+          <input type="checkbox" className="ml-[8px] flex-1" />
+        </li>
+        <li>
+          <img
+            src="../../../cart/cartCover.jpeg"
+            className=" ml-[20px] mr-[50px] h-[60px] w-[60px]"
+          />
+        </li>
+        <li className="mr-[70px] flex flex-col items-center">
+          <div>여성초 스팟 패드</div>
+          <div>카밍 터치</div>
+          <div>
+            <img
+              src="../../../cart/cartSale.png"
+              className="h-[19px] w-[45px]"
+            />
+          </div>
+        </li>
+        <li className="mr-[50px]">
+          <p>₩24,000</p>
+          <p className="font-bold">₩16,800</p>
+        </li>
+        <li className="mr-[105px]">
+          <input type="text" className="w-[50px]" />2
+        </li>
+        <li className="mr-[72px]">₩1,680</li>
+        <li className="mr-[75px] text-center">
+          <p>2,500</p>
+          <p>조건</p>
+        </li>
+        <li className="font-bold">33,600</li>
+        <li>
+          <button
+            type="button"
+            className="ml-[24px] h-[35px] w-[60px] border border-solid border-black font-bold"
+          >
+            삭제
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default CartProduct;
